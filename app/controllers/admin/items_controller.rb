5b2709c6b676c0eebda19aa976a6d21212fe0b4e
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-06-12 02:16:08 +0000

    [items edit]EC

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -22,7 +22,7 @@ class Admin::ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
-    redirect_to admin_item_path(item.id)
+    redirect_to admin_item_path
   end
 
   def edit
