7d68fb27b4b6b0f613e8b609f7f6919b2101ba65
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-06-13 14:30:55 +0000

    [admin]EC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'orders/show'
+  end
   namespace :public do
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
