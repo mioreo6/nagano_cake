d7d8bc7d8431cee404343982cfd9fe4f4688cd74
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-06-14 13:50:23 +0000

    [admin order]EC

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  has_many :cart_items, dependent: :destroy
+  has_many :customer, through: :cart_items
+  has_many :orders, through: :order_details
+  belongs_to :genre
 
   def add_tax_price
       (self.price * 1.10).round
