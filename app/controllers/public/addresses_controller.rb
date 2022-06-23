009a5355714e3055f3fc8aa957febd38c6c68191
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-06-23 14:46:10 +0000

    [address]EC]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,20 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to public_addresses_path
   end
 
   def edit
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
