e13a0a7b291c87019182bfca013c60354d274366
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-06-27 12:59:28 +0000

    [sign_in]EC

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -20,20 +20,22 @@ class Public::SessionsController < Devise::SessionsController
 
    protected
     def customer_state
-  ## 【処理内容1】 入力されたemailからアカウントを1件取得
+
     @customer = Customer.find_by(email: params[:customer][:email])
-   ## アカウントを取得できなかった場合、このメソッドを終了する
-    return if !@customer
-  ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    if @customer.valid_password?(params[:customer][:password]) && !(@user.is_deleted)
-    ## 【処理内容3】
+
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+
      flash[:notice] = "退会済みです。再登録をしてください。"
      redirect_to  new_customer_registration_path
-    else
-     Customer.create
-     render :create
     end
+
     end
+
+    def after_sign_in_path_for(resource)
+     public_customer_path(current_customer)
+    end
+
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
