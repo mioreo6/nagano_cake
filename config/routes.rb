f08aa16c248f9356c2c9577e5ec977f14e940013
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-07-03 09:01:57 +0000

    [customers edit]EC

--- config/routes.rb
+++ config/routes.rb
@@ -23,11 +23,6 @@ Rails.application.routes.draw do
    root to: "homes#top"
   end
 
-  devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-
-  }
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
@@ -53,6 +48,11 @@ Rails.application.routes.draw do
   get 'customers/unsubscribe' => 'public/customers#unsubscribe', as: 'unsubscribe'
   patch 'customers/withdraw' => 'public/customers#withdraw', as: 'withdraw'
 end
+
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations", only: [:new, :create],
+  sessions: 'public/sessions'
+  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
