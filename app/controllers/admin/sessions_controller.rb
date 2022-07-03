add9831933039ac299a1843aafafc4d7c959966d
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-07-03 08:08:54 +0000

    [about]EC

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-
+ before_action :authenticate_admin!, except: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
