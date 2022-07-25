e9e3c70b99337e28f6bba63b8a19e688eecd7492
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-07-25 11:09:35 +0000

    [Finish]EC

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,7 +2,7 @@ class Public::AddressesController < ApplicationController
  before_action :authenticate_customer!
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
 
 
@@ -18,7 +18,7 @@ class Public::AddressesController < ApplicationController
     render :index
     end
   end
-  
+
   def destroy
     address = Address.find(params[:id])
     address.destroy
