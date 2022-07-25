5af1f4dd39e473a8a279b7e93526a1ad13503d2b
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-07-25 12:07:32 +0000

    [Finish]EC

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  
+
   before_action :authenticate_customer!
   def show
     @customer = current_customer
@@ -24,6 +24,6 @@ class Public::CustomersController < ApplicationController
 
   private
   def customer_params
-   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address,  :telephone_number,)
+   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address,  :telephone_number, :email)
   end
 end
